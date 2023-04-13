88c818579ae2384f35d1256f045bbbf5bea8a7ff
Samantha <51409073+Tari38@users.noreply.github.com>
2023-04-13 17:55:13 +0100

    added fees page

--- src/pages/Fees.jsx
+++ src/pages/Fees.jsx
@@ -1,48 +1,23 @@
+import FeesCards from "../components/cards/Fees";
+import "./css/Fees.css";
+
 export default function Fees() {
     return (
-        <div>
-            
-            ZKS MA PRICING.
-ZKS Martial Arts offer great value for money on all our membership tariffs. We always offer a FREE Trial to those interested in our classes and we don't take any payments up front. If you decide that you like our classes then the next step is to send back the Direct Debit form , which is a simple and easy way to pay. We do charge a one off Joiners fee of £25.00 to our 3 - 4 year old members and all other members we charge a £30 joining fee. We offer family discounts and you may find special promotions on our Facebook page.
+        <>
+        <section id="fees-hero">            
+            <h1>ZKS Martial Arts Fees</h1>
+            <p>ZKS Martial Arts offer great value for money on all our membership tariffs!</p>
+                
+            <p>We always offer a FREE Trial to those interested in our classes and we don't take any payments up front.</p>
+                
+            <p>If you decide that you like our classes then the next step is to send back the Direct Debit form , which is a simple and easy way to pay.</p> 
+                
+            <p>We do charge a one off Joiners fee of £25.00 to our 3 - 4 year old members and all other members we charge a £30 joining fee. We offer family discounts and you may find special promotions on our Facebook page.</p>
 
-STANDARD
-Start from
-
-£30.00
-per month
-
-One Class a week
-Family Discounts
-available.
-PREMIUM
-Start from
-
-£45.00
-per month
-
-Attend Two
-or more
-classes a week.
-DRAGONS JOINERS PACK
-£25.00
-One off joining Fee.
-
-A ZKS Kit Bag
-A ZKS Water Bottle
-A ZKS T Shirt
-Dragons Sticker Chart
-Class Information pack
-Membership set up fee
-JUNIOR JOINERS PACK
-£30.00
-One off joining Fee.
-
-A ZKS Kit Bag
-A ZKS Water Bottle
-A ZKS T Shirt
-Sparring Gloves
-Class Information pack
-Membership set up fee
-        </div>
+        </section>
+        <section id="card-fees">
+            <FeesCards />
+        </section>
+        </>
     )
 }
